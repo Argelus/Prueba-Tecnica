25602193f8e8be3478f458058e0c613166298ee5
Anghelo Ortiz <anghelotiz@gmail.com>
2025-12-05 16:26:23 -0600

    Se agrega swagger para documentar la API

--- demo/demo/src/main/java/com/tickets/demo/DemoApplication.java
+++ demo/demo/src/main/java/com/tickets/demo/DemoApplication.java
@@ -2,6 +2,9 @@ package com.tickets.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +13,12 @@ public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
     System.out.println("--- Servidor de Tickets iniciado en el puerto 8080 ---");
 }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Sistema de Tickets")
+                        .version("1.0")
+                        .description("Documentación oficial para el sistema de To-Do List y Tickets"));
+    }
 }
